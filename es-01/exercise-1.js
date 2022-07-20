276e4187730b301f982a5bf3174773781121111d
Milton Mariani <miltonmariani7@gmail.com>
2022-07-20 19:48:31 +0200

    Commit 0

---
+++ es-01/exercise-1.js
@@ -0,0 +1,27 @@
+
+
+let personName = 'Paul';
+
+
+function canPlay() {
+
+      if (personName === 'Paul') {
+        console.log(personName += ' plays football')
+    }  else {
+      console.log('Call another friend with who play');
+    }
+
+  }
+
+canPlay();
+
+
+
+
+
+
+
+
+
+
+
---
+++ es-02/exercise-2.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+function canPlay() {
+
+  const personName = {
+    abc: 'Paul'
+  };
+
+  if (true) {
+      personName = 'George'; // Argh! Voglio che personName sia una variabile di sola lettura
+  }
+
+
+
+  personName += ' plays football';
+
+  console.log(personName);
+
+}
+
+canPlay();
+
+
+
+
+
+const gatto = {
+    razza: persiana,
+    nome: pepi,
+    colore: grigio,
+
+};
+
+
+const persona = {
+  nome: luca,
+  altezza: 1.70,
+  occhi: neri,
+  capelli: neri,
+  hobby: bass-fishing
+};
+
+
+const bambino = '133';
+
+
+
+
