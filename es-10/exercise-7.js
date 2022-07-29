5eacb040baff6f5503946764d16ebd18b88315d3
Milton Mariani <miltonmariani7@gmail.com>
2022-07-29 20:34:42 +0200

    commit 3

---
+++ es-10/exercise-7.js
@@ -0,0 +1,89 @@
+
+
+function sumUntil(maxValue) {
+
+  let sum = 0;
+
+  for(let i = 0; i <= maxValue; i++) {
+       sum = sum + i;
+  }
+   return sum;
+   
+}
+
+
+console.log(sumUntil(5));
+
+
+
+
+
+
+
+// function sumUntil(maxValue) {
+//   for(let i = 0; i < maxValue.length; i++) {
+
+//   }
+//    return maxValue;
+// }
+
+
+// 0 + 1 + 2 + 3 + 4 + 5 = 15
+
+// console.log(sumUntil(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ es-11/exercise-1.js
@@ -0,0 +1,36 @@
+const sum =  (wella, peppa) => {
+  return wella + peppa;
+}
+
+console.log(sum(2, 5));
+
+
+
+const log =  (デカバス, ハンター) => {
+   return デカバス + ハンター;
+}
+
+console.log(log(2, 5));
+
+
+console.log(sum);
+console.log(log);
+
+
+
+
+
+
+
+
+/* ´${} ´ */
+
+
+
+
+
+
+
+
+
+
