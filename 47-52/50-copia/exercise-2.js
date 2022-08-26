0e235fbcd3f20316410016f28ea39f28e4face6e
Milton Mariani <miltonmariani7@gmail.com>
2022-08-26 18:16:11 +0200

    cartella 47-52

---
+++ 47-52/50-copia/exercise-2.js
@@ -0,0 +1,80 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+
+  static fromJson(x) {
+    return JSON.parse(x);
+  }
+}
+
+
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+
+const developer = Person.fromJson(json);
+
+console.log(developer);
+
+
+const develosthope =  JSON.stringify(developer);
+
+console.log(develosthope);
+
+const peppino = JSON.parse(develosthope);
+
+console.log(peppino);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
